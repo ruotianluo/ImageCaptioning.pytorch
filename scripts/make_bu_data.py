19d8d616d0ed99ae9879ad0a7ed5950b330cbe86
Ruotian Luo <rluo@ttic.edu>
2018-04-28 18:06:38 -0500

    Add options and verbose for make_bu_data.

--- scripts/make_bu_data.py
+++ scripts/make_bu_data.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import os
 import base64
 import numpy as np
@@ -6,25 +10,43 @@ import sys
 import zlib
 import time
 import mmap
+import argparse
+
+parser = argparse.ArgumentParser()
+
+# output_dir
+parser.add_argument('--downloaded_feats', default='data/bu_data', help='downloaded feature directory')
+parser.add_argument('--output_dir', default='data/cocobu', help='output feature files')
+
+args = parser.parse_args()
 
 csv.field_size_limit(sys.maxsize)
 
 
 FIELDNAMES = ['image_id', 'image_w','image_h','num_boxes', 'boxes', 'features']
-#infiles = ['trainval/karpathy_test_resnet101_faster_rcnn_genome.tsv',
-#          'trainval/karpathy_val_resnet101_faster_rcnn_genome.tsv',
-infiles = ['trainval/karpathy_train_resnet101_faster_rcnn_genome.tsv.0', \
+infiles = ['trainval/karpathy_test_resnet101_faster_rcnn_genome.tsv',
+          'trainval/karpathy_val_resnet101_faster_rcnn_genome.tsv',\
+          'trainval/karpathy_train_resnet101_faster_rcnn_genome.tsv.0', \
            'trainval/karpathy_train_resnet101_faster_rcnn_genome.tsv.1']
 
+os.makedirs(args.output_dir+'_att')
+os.makedirs(args.output_dir+'_fc')
+os.makedirs(args.output_dir+'_box')
+
 for infile in infiles:
-    with open(infile, "r+b") as tsv_in_file:
+    print('Reading ' + infile)
+    with open(os.path.join(args.downloaded_feats, infile), "r+b") as tsv_in_file:
         reader = csv.DictReader(tsv_in_file, delimiter='\t', fieldnames = FIELDNAMES)
         for item in reader:
             item['image_id'] = int(item['image_id'])
             item['num_boxes'] = int(item['num_boxes'])
             for field in ['boxes', 'features']:
                 item[field] = np.frombuffer(base64.decodestring(item[field]), 
-                      dtype=np.float32).reshape((item['num_boxes'],-1))
-            np.savez_compressed(os.path.join('../cocobu_att', str(item['image_id'])), feat=item['features'])
-            np.save(os.path.join('../cocobu_fc', str(item['image_id'])), item['features'].mean(0))
+                        dtype=np.float32).reshape((item['num_boxes'],-1))
+            np.savez_compressed(os.path.join(args.output_dir+'_att', str(item['image_id'])), feat=item['features'])
+            np.save(os.path.join(args.output_dir+'_fc', str(item['image_id'])), item['features'].mean(0))
+            np.save(os.path.join(args.output_dir+'_box', str(item['image_id'])), item['boxes'])
+
+
+
 
