f0b12c6a7f83762c599adbc61623419d2f932911
Ruotian Luo <rluo@ttic.edu>
2018-05-18 14:03:53 -0500

    [0.5]Match the new arange behaior.

--- models/TransformerModel.py
+++ models/TransformerModel.py
@@ -228,8 +228,8 @@ class PositionalEncoding(nn.Module):
         
         # Compute the positional encodings once in log space.
         pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2) *
+        position = torch.arange(0, max_len).unsqueeze(1).float()
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() *
                              -(math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
