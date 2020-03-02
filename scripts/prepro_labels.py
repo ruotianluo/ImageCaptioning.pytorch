82b1ee0ababc401aeb91f5ee1cba2e79b3065efc
Ruotian Luo <rluo@ttic.edu>
2020-03-02 13:43:11 -0600

    [Minor] formating.

--- scripts/prepro_labels.py
+++ scripts/prepro_labels.py
@@ -39,165 +39,168 @@ import torchvision.models as models
 import skimage.io
 from PIL import Image
 
+
 def build_vocab(imgs, params):
-	count_thr = params['word_count_threshold']
+    count_thr = params['word_count_threshold']
 
-	# count up the number of words
-	counts = {}
-	for img in imgs:
-		for sent in img['sentences']:
-			for w in sent['tokens']:
-				counts[w] = counts.get(w, 0) + 1
-	cw = sorted([(count,w) for w,count in counts.items()], reverse=True)
-	print('top words and their counts:')
-	print('\n'.join(map(str,cw[:20])))
+    # count up the number of words
+    counts = {}
+    for img in imgs:
+        for sent in img['sentences']:
+            for w in sent['tokens']:
+                counts[w] = counts.get(w, 0) + 1
+    cw = sorted([(count,w) for w,count in counts.items()], reverse=True)
+    print('top words and their counts:')
+    print('\n'.join(map(str,cw[:20])))
 
-	# print some stats
-	total_words = sum(counts.values())
-	print('total words:', total_words)
-	bad_words = [w for w,n in counts.items() if n <= count_thr]
-	vocab = [w for w,n in counts.items() if n > count_thr]
-	bad_count = sum(counts[w] for w in bad_words)
-	print('number of bad words: %d/%d = %.2f%%' % (len(bad_words), len(counts), len(bad_words)*100.0/len(counts)))
-	print('number of words in vocab would be %d' % (len(vocab), ))
-	print('number of UNKs: %d/%d = %.2f%%' % (bad_count, total_words, bad_count*100.0/total_words))
+    # print some stats
+    total_words = sum(counts.values())
+    print('total words:', total_words)
+    bad_words = [w for w,n in counts.items() if n <= count_thr]
+    vocab = [w for w,n in counts.items() if n > count_thr]
+    bad_count = sum(counts[w] for w in bad_words)
+    print('number of bad words: %d/%d = %.2f%%' % (len(bad_words), len(counts), len(bad_words)*100.0/len(counts)))
+    print('number of words in vocab would be %d' % (len(vocab), ))
+    print('number of UNKs: %d/%d = %.2f%%' % (bad_count, total_words, bad_count*100.0/total_words))
 
-	# lets look at the distribution of lengths as well
-	sent_lengths = {}
-	for img in imgs:
-		for sent in img['sentences']:
-			txt = sent['tokens']
-			nw = len(txt)
-			sent_lengths[nw] = sent_lengths.get(nw, 0) + 1
-	max_len = max(sent_lengths.keys())
-	print('max length sentence in raw data: ', max_len)
-	print('sentence length distribution (count, number of words):')
-	sum_len = sum(sent_lengths.values())
-	for i in range(max_len+1):
-		print('%2d: %10d   %f%%' % (i, sent_lengths.get(i,0), sent_lengths.get(i,0)*100.0/sum_len))
+    # lets look at the distribution of lengths as well
+    sent_lengths = {}
+    for img in imgs:
+        for sent in img['sentences']:
+            txt = sent['tokens']
+            nw = len(txt)
+            sent_lengths[nw] = sent_lengths.get(nw, 0) + 1
+    max_len = max(sent_lengths.keys())
+    print('max length sentence in raw data: ', max_len)
+    print('sentence length distribution (count, number of words):')
+    sum_len = sum(sent_lengths.values())
+    for i in range(max_len+1):
+        print('%2d: %10d   %f%%' % (i, sent_lengths.get(i,0), sent_lengths.get(i,0)*100.0/sum_len))
 
-	# lets now produce the final annotations
-	if bad_count > 0:
-		# additional special UNK token we will use below to map infrequent words to
-		print('inserting the special UNK token')
-		vocab.append('UNK')
-	
-	for img in imgs:
-		img['final_captions'] = []
-		for sent in img['sentences']:
-			txt = sent['tokens']
-			caption = [w if counts.get(w,0) > count_thr else 'UNK' for w in txt]
-			img['final_captions'].append(caption)
+    # lets now produce the final annotations
+    if bad_count > 0:
+        # additional special UNK token we will use below to map infrequent words to
+        print('inserting the special UNK token')
+        vocab.append('UNK')
+    
+    for img in imgs:
+        img['final_captions'] = []
+        for sent in img['sentences']:
+            txt = sent['tokens']
+            caption = [w if counts.get(w,0) > count_thr else 'UNK' for w in txt]
+            img['final_captions'].append(caption)
 
-	return vocab
+    return vocab
+
 
 def encode_captions(imgs, params, wtoi):
-	""" 
-	encode all captions into one large array, which will be 1-indexed.
-	also produces label_start_ix and label_end_ix which store 1-indexed 
-	and inclusive (Lua-style) pointers to the first and last caption for
-	each image in the dataset.
-	"""
+    """ 
+    encode all captions into one large array, which will be 1-indexed.
+    also produces label_start_ix and label_end_ix which store 1-indexed 
+    and inclusive (Lua-style) pointers to the first and last caption for
+    each image in the dataset.
+    """
 
-	max_length = params['max_length']
-	N = len(imgs)
-	M = sum(len(img['final_captions']) for img in imgs) # total number of captions
+    max_length = params['max_length']
+    N = len(imgs)
+    M = sum(len(img['final_captions']) for img in imgs) # total number of captions
 
-	label_arrays = []
-	label_start_ix = np.zeros(N, dtype='uint32') # note: these will be one-indexed
-	label_end_ix = np.zeros(N, dtype='uint32')
-	label_length = np.zeros(M, dtype='uint32')
-	caption_counter = 0
-	counter = 1
-	for i,img in enumerate(imgs):
-		n = len(img['final_captions'])
-		assert n > 0, 'error: some image has no captions'
+    label_arrays = []
+    label_start_ix = np.zeros(N, dtype='uint32') # note: these will be one-indexed
+    label_end_ix = np.zeros(N, dtype='uint32')
+    label_length = np.zeros(M, dtype='uint32')
+    caption_counter = 0
+    counter = 1
+    for i,img in enumerate(imgs):
+        n = len(img['final_captions'])
+        assert n > 0, 'error: some image has no captions'
 
-		Li = np.zeros((n, max_length), dtype='uint32')
-		for j,s in enumerate(img['final_captions']):
-			label_length[caption_counter] = min(max_length, len(s)) # record the length of this sequence
-			caption_counter += 1
-			for k,w in enumerate(s):
-				if k < max_length:
-					Li[j,k] = wtoi[w]
+        Li = np.zeros((n, max_length), dtype='uint32')
+        for j,s in enumerate(img['final_captions']):
+            label_length[caption_counter] = min(max_length, len(s)) # record the length of this sequence
+            caption_counter += 1
+            for k,w in enumerate(s):
+                if k < max_length:
+                    Li[j,k] = wtoi[w]
 
-		# note: word indices are 1-indexed, and captions are padded with zeros
-		label_arrays.append(Li)
-		label_start_ix[i] = counter
-		label_end_ix[i] = counter + n - 1
-		
-		counter += n
-	
-	L = np.concatenate(label_arrays, axis=0) # put all the labels together
-	assert L.shape[0] == M, 'lengths don\'t match? that\'s weird'
-	assert np.all(label_length > 0), 'error: some caption had no words?'
+        # note: word indices are 1-indexed, and captions are padded with zeros
+        label_arrays.append(Li)
+        label_start_ix[i] = counter
+        label_end_ix[i] = counter + n - 1
+        
+        counter += n
+    
+    L = np.concatenate(label_arrays, axis=0) # put all the labels together
+    assert L.shape[0] == M, 'lengths don\'t match? that\'s weird'
+    assert np.all(label_length > 0), 'error: some caption had no words?'
 
-	print('encoded captions to array of size ', L.shape)
-	return L, label_start_ix, label_end_ix, label_length
+    print('encoded captions to array of size ', L.shape)
+    return L, label_start_ix, label_end_ix, label_length
+
 
 def main(params):
 
-	imgs = json.load(open(params['input_json'], 'r'))
-	imgs = imgs['images']
+    imgs = json.load(open(params['input_json'], 'r'))
+    imgs = imgs['images']
 
-	seed(123) # make reproducible
-	
-	# create the vocab
-	vocab = build_vocab(imgs, params)
-	itow = {i+1:w for i,w in enumerate(vocab)} # a 1-indexed vocab translation table
-	wtoi = {w:i+1 for i,w in enumerate(vocab)} # inverse table
-	
-	# encode captions in large arrays, ready to ship to hdf5 file
-	L, label_start_ix, label_end_ix, label_length = encode_captions(imgs, params, wtoi)
+    seed(123) # make reproducible
+    
+    # create the vocab
+    vocab = build_vocab(imgs, params)
+    itow = {i+1:w for i,w in enumerate(vocab)} # a 1-indexed vocab translation table
+    wtoi = {w:i+1 for i,w in enumerate(vocab)} # inverse table
+    
+    # encode captions in large arrays, ready to ship to hdf5 file
+    L, label_start_ix, label_end_ix, label_length = encode_captions(imgs, params, wtoi)
 
-	# create output h5 file
-	N = len(imgs)
-	f_lb = h5py.File(params['output_h5']+'_label.h5', "w")
-	f_lb.create_dataset("labels", dtype='uint32', data=L)
-	f_lb.create_dataset("label_start_ix", dtype='uint32', data=label_start_ix)
-	f_lb.create_dataset("label_end_ix", dtype='uint32', data=label_end_ix)
-	f_lb.create_dataset("label_length", dtype='uint32', data=label_length)
-	f_lb.close()
+    # create output h5 file
+    N = len(imgs)
+    f_lb = h5py.File(params['output_h5']+'_label.h5', "w")
+    f_lb.create_dataset("labels", dtype='uint32', data=L)
+    f_lb.create_dataset("label_start_ix", dtype='uint32', data=label_start_ix)
+    f_lb.create_dataset("label_end_ix", dtype='uint32', data=label_end_ix)
+    f_lb.create_dataset("label_length", dtype='uint32', data=label_length)
+    f_lb.close()
 
-	# create output json file
-	out = {}
-	out['ix_to_word'] = itow # encode the (1-indexed) vocab
-	out['images'] = []
-	for i,img in enumerate(imgs):
-		
-		jimg = {}
-		jimg['split'] = img['split']
-		if 'filename' in img: jimg['file_path'] = os.path.join(img.get('filepath', ''), img['filename']) # copy it over, might need
-		if 'cocoid' in img:
-			jimg['id'] = img['cocoid'] # copy over & mantain an id, if present (e.g. coco ids, useful)
-		elif 'imgid' in img:
-			jimg['id'] = img['imgid']
+    # create output json file
+    out = {}
+    out['ix_to_word'] = itow # encode the (1-indexed) vocab
+    out['images'] = []
+    for i,img in enumerate(imgs):
+        
+        jimg = {}
+        jimg['split'] = img['split']
+        if 'filename' in img: jimg['file_path'] = os.path.join(img.get('filepath', ''), img['filename']) # copy it over, might need
+        if 'cocoid' in img:
+            jimg['id'] = img['cocoid'] # copy over & mantain an id, if present (e.g. coco ids, useful)
+        elif 'imgid' in img:
+            jimg['id'] = img['imgid']
 
-		if params['images_root'] != '':
-			with Image.open(os.path.join(params['images_root'], img['filepath'], img['filename'])) as _img:
-				jimg['width'], jimg['height'] = _img.size
+        if params['images_root'] != '':
+            with Image.open(os.path.join(params['images_root'], img['filepath'], img['filename'])) as _img:
+                jimg['width'], jimg['height'] = _img.size
 
-		out['images'].append(jimg)
-	
-	json.dump(out, open(params['output_json'], 'w'))
-	print('wrote ', params['output_json'])
+        out['images'].append(jimg)
+    
+    json.dump(out, open(params['output_json'], 'w'))
+    print('wrote ', params['output_json'])
 
 if __name__ == "__main__":
 
-	parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-	# input json
-	parser.add_argument('--input_json', required=True, help='input json file to process into hdf5')
-	parser.add_argument('--output_json', default='data.json', help='output json file')
-	parser.add_argument('--output_h5', default='data', help='output h5 file')
-	parser.add_argument('--images_root', default='', help='root location in which images are stored, to be prepended to file_path in input json')
+    # input json
+    parser.add_argument('--input_json', required=True, help='input json file to process into hdf5')
+    parser.add_argument('--output_json', default='data.json', help='output json file')
+    parser.add_argument('--output_h5', default='data', help='output h5 file')
+    parser.add_argument('--images_root', default='', help='root location in which images are stored, to be prepended to file_path in input json')
 
-	# options
-	parser.add_argument('--max_length', default=16, type=int, help='max length of a caption, in number of words. captions longer than this get clipped.')
-	parser.add_argument('--word_count_threshold', default=5, type=int, help='only words that occur more than this number of times will be put in vocab')
+    # options
+    parser.add_argument('--max_length', default=16, type=int, help='max length of a caption, in number of words. captions longer than this get clipped.')
+    parser.add_argument('--word_count_threshold', default=5, type=int, help='only words that occur more than this number of times will be put in vocab')
 
-	args = parser.parse_args()
-	params = vars(args) # convert to ordinary dict
-	print('parsed input parameters:')
-	print(json.dumps(params, indent = 2))
-	main(params)
+    args = parser.parse_args()
+    params = vars(args) # convert to ordinary dict
+    print('parsed input parameters:')
+    print(json.dumps(params, indent = 2))
+    main(params)
--- scripts/prepro_ngrams.py
+++ scripts/prepro_ngrams.py
@@ -10,120 +10,120 @@ import misc.utils as utils
 from collections import defaultdict
 
 def precook(s, n=4, out=False):
-	"""
-	Takes a string as input and returns an object that can be given to
-	either cook_refs or cook_test. This is optional: cook_refs and cook_test
-	can take string arguments as well.
-	:param s: string : sentence to be converted into ngrams
-	:param n: int    : number of ngrams for which representation is calculated
-	:return: term frequency vector for occuring ngrams
-	"""
-	words = s.split()
-	counts = defaultdict(int)
-	for k in range(1,n+1):
-		for i in range(len(words)-k+1):
-			ngram = tuple(words[i:i+k])
-			counts[ngram] += 1
-	return counts
+    """
+    Takes a string as input and returns an object that can be given to
+    either cook_refs or cook_test. This is optional: cook_refs and cook_test
+    can take string arguments as well.
+    :param s: string : sentence to be converted into ngrams
+    :param n: int    : number of ngrams for which representation is calculated
+    :return: term frequency vector for occuring ngrams
+    """
+    words = s.split()
+    counts = defaultdict(int)
+    for k in range(1,n+1):
+        for i in range(len(words)-k+1):
+            ngram = tuple(words[i:i+k])
+            counts[ngram] += 1
+    return counts
 
 def cook_refs(refs, n=4): ## lhuang: oracle will call with "average"
-		'''Takes a list of reference sentences for a single segment
-		and returns an object that encapsulates everything that BLEU
-		needs to know about them.
-		:param refs: list of string : reference sentences for some image
-		:param n: int : number of ngrams for which (ngram) representation is calculated
-		:return: result (list of dict)
-		'''
-		return [precook(ref, n) for ref in refs]
+    '''Takes a list of reference sentences for a single segment
+    and returns an object that encapsulates everything that BLEU
+    needs to know about them.
+    :param refs: list of string : reference sentences for some image
+    :param n: int : number of ngrams for which (ngram) representation is calculated
+    :return: result (list of dict)
+    '''
+    return [precook(ref, n) for ref in refs]
 
 def create_crefs(refs):
-	crefs = []
-	for ref in refs:
-		# ref is a list of 5 captions
-		crefs.append(cook_refs(ref))
-	return crefs
+    crefs = []
+    for ref in refs:
+        # ref is a list of 5 captions
+        crefs.append(cook_refs(ref))
+    return crefs
 
 def compute_doc_freq(crefs):
-	'''
-	Compute term frequency for reference data.
-	This will be used to compute idf (inverse document frequency later)
-	The term frequency is stored in the object
-	:return: None
-	'''
-	document_frequency = defaultdict(float)
-	for refs in crefs:
-		# refs, k ref captions of one image
-		for ngram in set([ngram for ref in refs for (ngram,count) in ref.items()]):
-			document_frequency[ngram] += 1
-			# maxcounts[ngram] = max(maxcounts.get(ngram,0), count)
-	return document_frequency
+    '''
+    Compute term frequency for reference data.
+    This will be used to compute idf (inverse document frequency later)
+    The term frequency is stored in the object
+    :return: None
+    '''
+    document_frequency = defaultdict(float)
+    for refs in crefs:
+        # refs, k ref captions of one image
+        for ngram in set([ngram for ref in refs for (ngram,count) in ref.items()]):
+            document_frequency[ngram] += 1
+            # maxcounts[ngram] = max(maxcounts.get(ngram,0), count)
+    return document_frequency
 
 def build_dict(imgs, wtoi, params):
-	wtoi['<eos>'] = 0
+    wtoi['<eos>'] = 0
 
-	count_imgs = 0
+    count_imgs = 0
 
-	refs_words = []
-	refs_idxs = []
-	for img in imgs:
-		if (params['split'] == img['split']) or \
-			(params['split'] == 'train' and img['split'] == 'restval') or \
-			(params['split'] == 'all'):
-			#(params['split'] == 'val' and img['split'] == 'restval') or \
-			ref_words = []
-			ref_idxs = []
-			for sent in img['sentences']:
-				if hasattr(params, 'bpe'):
-					sent['tokens'] = params.bpe.segment(' '.join(sent['tokens'])).strip().split(' ')
-				tmp_tokens = sent['tokens'] + ['<eos>']
-				tmp_tokens = [_ if _ in wtoi else 'UNK' for _ in tmp_tokens]
-				ref_words.append(' '.join(tmp_tokens))
-				ref_idxs.append(' '.join([str(wtoi[_]) for _ in tmp_tokens]))
-			refs_words.append(ref_words)
-			refs_idxs.append(ref_idxs)
-			count_imgs += 1
-	print('total imgs:', count_imgs)
+    refs_words = []
+    refs_idxs = []
+    for img in imgs:
+        if (params['split'] == img['split']) or \
+            (params['split'] == 'train' and img['split'] == 'restval') or \
+            (params['split'] == 'all'):
+            #(params['split'] == 'val' and img['split'] == 'restval') or \
+            ref_words = []
+            ref_idxs = []
+            for sent in img['sentences']:
+                if hasattr(params, 'bpe'):
+                    sent['tokens'] = params.bpe.segment(' '.join(sent['tokens'])).strip().split(' ')
+                tmp_tokens = sent['tokens'] + ['<eos>']
+                tmp_tokens = [_ if _ in wtoi else 'UNK' for _ in tmp_tokens]
+                ref_words.append(' '.join(tmp_tokens))
+                ref_idxs.append(' '.join([str(wtoi[_]) for _ in tmp_tokens]))
+            refs_words.append(ref_words)
+            refs_idxs.append(ref_idxs)
+            count_imgs += 1
+    print('total imgs:', count_imgs)
 
-	ngram_words = compute_doc_freq(create_crefs(refs_words))
-	ngram_idxs = compute_doc_freq(create_crefs(refs_idxs))
-	return ngram_words, ngram_idxs, count_imgs
+    ngram_words = compute_doc_freq(create_crefs(refs_words))
+    ngram_idxs = compute_doc_freq(create_crefs(refs_idxs))
+    return ngram_words, ngram_idxs, count_imgs
 
 def main(params):
 
-	imgs = json.load(open(params['input_json'], 'r'))
-	dict_json = json.load(open(params['dict_json'], 'r'))
-	itow = dict_json['ix_to_word']
-	wtoi = {w:i for i,w in itow.items()}
+    imgs = json.load(open(params['input_json'], 'r'))
+    dict_json = json.load(open(params['dict_json'], 'r'))
+    itow = dict_json['ix_to_word']
+    wtoi = {w:i for i,w in itow.items()}
 
-	# Load bpe
-	if 'bpe' in dict_json:
-		import tempfile
-		import codecs
-		codes_f = tempfile.NamedTemporaryFile(delete=False)
-		codes_f.close()
-		with open(codes_f.name, 'w') as f:
-			f.write(dict_json['bpe'])
-		with codecs.open(codes_f.name, encoding='UTF-8') as codes:
-			bpe = apply_bpe.BPE(codes)
-		params.bpe = bpe
+    # Load bpe
+    if 'bpe' in dict_json:
+        import tempfile
+        import codecs
+        codes_f = tempfile.NamedTemporaryFile(delete=False)
+        codes_f.close()
+        with open(codes_f.name, 'w') as f:
+            f.write(dict_json['bpe'])
+        with codecs.open(codes_f.name, encoding='UTF-8') as codes:
+            bpe = apply_bpe.BPE(codes)
+        params.bpe = bpe
 
-	imgs = imgs['images']
+    imgs = imgs['images']
 
-	ngram_words, ngram_idxs, ref_len = build_dict(imgs, wtoi, params)
+    ngram_words, ngram_idxs, ref_len = build_dict(imgs, wtoi, params)
 
-	utils.pickle_dump({'document_frequency': ngram_words, 'ref_len': ref_len}, open(params['output_pkl']+'-words.p','wb'))
-	utils.pickle_dump({'document_frequency': ngram_idxs, 'ref_len': ref_len}, open(params['output_pkl']+'-idxs.p','wb'))
+    utils.pickle_dump({'document_frequency': ngram_words, 'ref_len': ref_len}, open(params['output_pkl']+'-words.p','wb'))
+    utils.pickle_dump({'document_frequency': ngram_idxs, 'ref_len': ref_len}, open(params['output_pkl']+'-idxs.p','wb'))
 
 if __name__ == "__main__":
 
-	parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-	# input json
-	parser.add_argument('--input_json', default='/home-nfs/rluo/rluo/nips/code/prepro/dataset_coco.json', help='input json file to process into hdf5')
-	parser.add_argument('--dict_json', default='data/cocotalk.json', help='output json file')
-	parser.add_argument('--output_pkl', default='data/coco-all', help='output pickle file')
-	parser.add_argument('--split', default='all', help='test, val, train, all')
-	args = parser.parse_args()
-	params = vars(args) # convert to ordinary dict
+    # input json
+    parser.add_argument('--input_json', default='/home-nfs/rluo/rluo/nips/code/prepro/dataset_coco.json', help='input json file to process into hdf5')
+    parser.add_argument('--dict_json', default='data/cocotalk.json', help='output json file')
+    parser.add_argument('--output_pkl', default='data/coco-all', help='output pickle file')
+    parser.add_argument('--split', default='all', help='test, val, train, all')
+    args = parser.parse_args()
+    params = vars(args) # convert to ordinary dict
 
-	main(params)
+    main(params)
