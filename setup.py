94fb160456858ef6d7fb842f35df7cfaa750c083
Ruotian Luo <rluo@ttic.edu>
2020-05-29 16:17:17 -0500

    MIP: step 2: add __init__.py and setup.py (it's a package now!)

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="captioning",
+    version="0.0.1",
+    author="Ruotian Luo",
+    author_email="rluo@ttic.edu",
+    packages=setuptools.find_packages(),
+)
