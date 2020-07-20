adacc50625713b229d96f8476d4ed3ac6af2d7a3
Ruotian Luo <rluo@ttic.edu>
2020-07-19 22:41:43 -0500

    fix overflow when mixed precision.

--- captioning/models/TransformerModel.py
+++ captioning/models/TransformerModel.py
@@ -155,7 +155,7 @@ def attention(query, key, value, mask=None, dropout=None):
     scores = torch.matmul(query, key.transpose(-2, -1)) \
              / math.sqrt(d_k)
     if mask is not None:
-        scores = scores.masked_fill(mask == 0, -1e9)
+        scores = scores.masked_fill(mask == 0, float('-inf'))
     p_attn = F.softmax(scores, dim = -1)
     if dropout is not None:
         p_attn = dropout(p_attn)
